5fa36cff5d14dd44f148702dc3535057dadb7f15
tszemzo <szemzotomas@gmail.com>
2021-09-30 15:58:48 -0300

    Moving errors and middlewares to common repo

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,8 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error';
+
+import { errorHandler, NotFoundError } from '@ts-tickets/common';
 
 const app = express();
 const isTestEnv = process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'local';
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ts-tickets/common';
 
 const router = express.Router();
 
