33f8bb31dbd9bcdfa1a6b1fe2af24eafd463e1e1
tszemzo <szemzotomas@gmail.com>
2022-01-09 19:53:40 -0300

    Add ticket update publisher event

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   validateRequest 
 } from '@ts-tickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -33,6 +35,13 @@ router.put('/api/tickets/:id', requireAuth,
     });
     // Persist data
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
+    
     res.send(ticket);
   }
 );
