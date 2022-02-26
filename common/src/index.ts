b726a7a0a799f84050683677629ca2c832900717
tszemzo <szemzotomas@gmail.com>
2022-02-26 18:31:04 -0300

    Just testing an update

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,4 @@
-// Re-export stuff from errors and middlewares
+// Re-export stuff from errors and middlewares.
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
